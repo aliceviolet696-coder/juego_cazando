51f9b9c135cbd14de55b711db5eb20cabc1bcf81
aliceviolet696 <aliceviolet696@gmail.com>
2026-04-22 16:05:08 -0500

    juego limones

--- limones.js
+++ limones.js
@@ -1,2 +1,39 @@
 let canvas=document.getElementById("areajuego");
 let ctx=canvas.getContext("2d");
+
+const ALTURA_SUELO=40;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2;
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
+
+function dibujarSuelo(){
+   ctx.fillStyle = "#5BCFFB";
+   ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+}
+function dibujarPersonaje(){
+    ctx.fillStyle="yellow";
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+}
+
+function moverDerecha(){
+    personajeX=personajeX+10;
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
